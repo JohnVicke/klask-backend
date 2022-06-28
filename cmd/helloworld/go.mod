2cec9728443564ae7aad48f47dbed6a02fe5c695
Viktor Malmedal <viktormalmedal@gmail.com>
2022-06-28 11:29:40 +0200

    test workflow only on push

--- cmd/helloworld/go.mod
+++ cmd/helloworld/go.mod
@@ -2,4 +2,4 @@ module github.com/JohnVicke/klask-backend/cmd/helloworld
 
 go 1.13
 
-require github.com/stretchr/testify v1.7.5 // indirect
+require github.com/stretchr/testify v1.7.5
--- main.go
+++ main.go
@@ -7,13 +7,25 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/JohnVicke/klask-backend/cmd/healthcheck"
 	"github.com/JohnVicke/klask-backend/cmd/helloworld"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	port := "8080"
+func init() {	
 
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error [godotenv] loading .env file: %v\n", err)
+	}
+	log.Println("Loaded .env file")
+}
+
+func main() {
 	funcframework.RegisterHTTPFunction("/hello-world", helloworld.HelloWorld)
 	funcframework.RegisterHTTPFunction("/healthcheck", healthcheck.HealthCheck)
+
+	port := "8080"
 
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		port = envPort
