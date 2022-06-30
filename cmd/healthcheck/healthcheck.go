0f1eaa7e3d59eb417e239e97b5852f8353dce771
Viktor Malmedal <viktormalmedal@gmail.com>
2022-06-30 13:42:48 +0200

    test

--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -3,7 +3,6 @@ package healthcheck
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
 func init() {
@@ -12,8 +11,7 @@ func init() {
 
 // HealthCheck is a function that returns a string.
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	var1 := os.Getenv("VAR1")
-	w.Write([]byte("Please ork!!!" + var1))
+	w.Write([]byte("Hello world!!!"))
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 }
--- cmd/helloworld/helloworld.go
+++ cmd/helloworld/helloworld.go
@@ -1,22 +1,12 @@
 package helloworld
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 // HelloWorld is a function that returns a string.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello, World!")
 	w.Write([]byte("Hello, World!"))
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 }
