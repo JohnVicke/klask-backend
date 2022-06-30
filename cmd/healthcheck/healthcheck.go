4c45159fbdef97262416821e652f73e038133d19
Viktor Malmedal <viktormalmedal@gmail.com>
2022-06-30 16:18:37 +0200

    test behaviour

--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -11,7 +11,7 @@ func init() {
 
 // HealthCheck is a function that returns a string.
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!!!"))
+	w.Write([]byte("Hello world!!"))
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 }
