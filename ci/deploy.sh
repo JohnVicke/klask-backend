2149a30717d3efe9c2e7e4852ca000d0c1f45a63
Viktor Malmedal <viktormalmedal@gmail.com>
2022-06-30 09:21:50 +0200

    test gcloud deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Using shell: $SHELL"
+
 files=("$@")
+
+VAR1=hello
 
 env_vars_array=(
   "VAR1=${VAR1}" 
@@ -12,16 +16,16 @@ env_vars=${joined%,}
 
 for file in "${files[@]}"
 do
-  echo "Verifying version $file"
-
   entry_point="$(grep -oP '(?<=func )\w+' $file)"
   name=${func_name,}
 
-#  gcloud functions deploy $name \
-#    --entry-point $entry_point \
-#    --set-env-vars $env_vars \
-#    --runtime go113 \
-#    --allow-unauthenticated \
-#    --trigger-http
+  echo "Deploying function $entry_point to gcloud..."
+
+  gcloud functions deploy $name \
+    --entry-point $entry_point \
+    --set-env-vars $env_vars \
+    --runtime go113 \
+    --allow-unauthenticated \
+    --trigger-http
 
 done
--- cmd/healthcheck/healthcheck.go
+++ cmd/healthcheck/healthcheck.go
@@ -1,9 +1,14 @@
 package healthcheck
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 // HealthCheck is a function that returns a string.
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	var1 := os.Getenv("VAR1")
+	w.Write([]byte("Hello World! " + var1))
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 }
