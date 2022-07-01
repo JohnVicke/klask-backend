3ac985ebf3edd600bb91882ce8a01edc1c6c15cf
Viktor Malmedal <38523983+JohnVicke@users.noreply.github.com>
2022-07-01 16:11:13 +0200

    feat/ add firebase client (#16)
    * docs: update dev workflow in README.md
    
    * feat: add function to init firebase admin

--- pkg/firebase/firebase.go
+++ pkg/firebase/firebase.go
@@ -1,5 +1,14 @@
 package firebase
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	"github.com/joho/godotenv"
+)
+
 // FirestoreUser is a user in Firestore
 type FirestoreUser struct {
 	Email string `firestore:"email" json:"email"`
@@ -15,4 +24,24 @@ type APIToken struct {
 	ExpiredAt string `firestore:"expiredAt" json:"expiredAt"`
 	Token string `firestore:"token" json:"token"`
 	ExpiresIn int `firestore:"expiresIn" json:"expiresIn"`
+}
+
+func init() {
+	godotenv.Load()
+	log.Println("Initialized firebase with .env file")
+}
+
+// InitializeFirebaseWithEnv initializes Firebase with the environment variables.
+func InitializeFirebaseWithEnv () (*firestore.Client, error) {
+	ctx := context.Background()
+	projectID := os.Getenv("FIREBASE_PROJECT_ID")
+
+	client, err := firestore.NewClient(ctx, projectID)
+
+	if err != nil {
+		log.Fatalf("error initializing firestore: %v\n", err)
+		return nil, err
+	}
+
+	return client, nil
 }
