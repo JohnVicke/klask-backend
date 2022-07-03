1d5b63bf83d14193dfab793d870d6ec11ad8782b
Viktor Malmedal <viktormalmedal@gmail.com>
2022-07-04 00:42:48 +0200

    exit subshell with return statement

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -8,15 +8,15 @@ run_tests_in_subdirs () {
       cd $dir
       go test ./... -v -cover
       if [ $? -ne 0 ]; then
-        echo "Tests in $dir failed"
-        exit $?
+        return 1
       fi
       cd ..
       printf "\n"
     fi
   done
   cd $2
+  return 0
 }
 
 base_dir=$(pwd)
-run_tests_in_subdirs "./cmd" $base_dir
+run_tests_in_subdirs "./cmd" $base_dir || exit 1
